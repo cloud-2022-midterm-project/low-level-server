caa4526d693055befe280c593453f2caa670b42f
Arm Sukrit <arm.xdsk@gmail.com>
2023-03-10 16:49:19 +0700

    refactor clearing db

--- src/handlers/clear.rs
+++ src/handlers/clear.rs
@@ -9,15 +9,10 @@ pub(crate) async fn clear(state: Arc<AppState>) -> String {
         .await;
 
     match result {
-        Ok(result) => {
-            if result.rows_affected() == 0 {
-                response.set_status_line("HTTP/1.1 404 NOT FOUND");
-            } else {
-                // remove from image store if it exists
-                state.image_store.clear();
-                state.mutations.lock().await.clear();
-                response.set_status_line("HTTP/1.1 204 NO CONTENT");
-            }
+        Ok(_) => {
+            state.image_store.clear();
+            state.mutations.lock().await.clear();
+            response.set_status_line("HTTP/1.1 204 NO CONTENT");
         }
         Err(_) => response.set_status_line("HTTP/1.1 500 INTERNAL SERVER ERROR"),
     }
